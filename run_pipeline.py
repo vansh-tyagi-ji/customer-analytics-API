fe304e2c04543ea1f657952c926fc1c18d822761
Vansh tyagi <vanstyagi596@gmail.com>
2025-10-31 16:10:36 +0530

    Update run_pipeline.py

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,36 +1,34 @@
-# file: run_pipeline.py
-
 from src.components.data_ingestion import DataIngestion
 from src.components.data_preprocessing import DataPreprocessing
 from src.utils.logger import logger
 
-# === Step 1: Data Ingestion Stage ===
+#  Data Ingestion Stage ===
 try:
     data_ingestion_component = DataIngestion()
 
-    # data_ingestion_component.process_task(task_name="churn_prediction")
+    data_ingestion_component.process_task(task_name="churn_prediction")
     data_ingestion_component.process_task(task_name="clv_prediction")
-    # data_ingestion_component.process_task(task_name="customer_segmentation")
+    data_ingestion_component.process_task(task_name="customer_segmentation")
     # data_ingestion_component.process_task(task_name="conversion_prediction")
     # data_ingestion_component.process_task(task_name="recommendation_system") 
-    logger.info(">>>>>>>>>> Stage 1: Data Ingestion Completed Successfully <<<<<<<<<<\n\n")
+    logger.info(" Stage 1: Data Ingestion Completed Successfully \n\n")
 
 except Exception as e:
     logger.error(f"Data Ingestion Stage Failed: {e}")
     raise e
 
 
-# === Step 2: Data Preprocessing Stage ===
+#  Data Preprocessing Stage 
 try:
-    logger.info(">>>>>>>>>> Stage 2: Data Preprocessing Started <<<<<<<<<<")
+    logger.info(" Stage 2: Data Preprocessing Started ")
     data_preprocessing_component = DataPreprocessing()
 
-    # data_preprocessing_component.preprocess_task(task_name="churn_prediction")
+    data_preprocessing_component.preprocess_task(task_name="churn_prediction")
     data_preprocessing_component.preprocess_task(task_name="clv_prediction")
-    # data_preprocessing_component.preprocess_task(task_name="customer_segmentation")
+    data_preprocessing_component.preprocess_task(task_name="customer_segmentation")
     # data_preprocessing_component.preprocess_task(task_name="conversion_prediction")
     
-    logger.info(">>>>>>>>>> Stage 2: Data Preprocessing Completed Successfully <<<<<<<<<<\n\n")
+    logger.info(" Stage 2: Data Preprocessing Completed Successfully \n\n")
 
 
 except Exception as e:
